1f1b4c5b3682ba42bd7742eeb839f0f9bcc1ed7a
tao-pipat <tao.pipat1995@gmail.com>
2020-07-17 13:51:06 +0700

    commit code passport

---
+++ app/Http/Controllers/API/CEOController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\CEO;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CEOResource;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class CEOController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $ceos = CEO::all();
+        return response(['ceos' => CEOResource::collection($ceos), 'message' => 'Retrieved successfully'], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required|max:255',
+            'year' => 'required|max:255',
+            'company_headquarters' => 'required|max:255',
+            'what_company_does' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response(['error' => $validator->errors(), 'Validation Error']);
+        }
+
+        $ceo = CEO::create($data);
+
+        return response(['ceo' => new CEOResource($ceo), 'message' => 'Created successfully'], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CEO  $cEO
+     * @return \Illuminate\Http\Response
+     */
+    public function show($cEO)
+    {
+        // CEO::find($cEO);
+        return response([ 'ceo' => new CEOResource(CEO::find($cEO)), 'message' => 'Retrieved successfully'], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CEO  $cEO
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,$cEO)
+    {
+        $ceo = CEO::find($cEO);
+        $ceo->update($request->all());
+        return response([ 'ceo' => new CEOResource($ceo), 'message' => 'Retrieved successfully'], 200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CEO  $cEO
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($cEO)
+    {
+        $ceo = CEO::find($cEO);
+        $ceo->delete();
+        return response(['message' => 'Deleted']);
+    }
+}
